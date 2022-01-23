b8ef3fb139a333149c775aa54a49f44842bb898b
iamsinghankit <34397119+iamsinghankit@users.noreply.github.com>
2022-01-23 12:39:52 +0530

    router added

--- src/main/java/com/github/reactivespring/model/Product.java
+++ src/main/java/com/github/reactivespring/model/Product.java
@@ -1,8 +1,7 @@
 package com.github.reactivespring.model;
 
-import org.springframework.data.annotation.Id;
-
 /**
  * @author iamsinghankit
  */
-public record Product(@Id Integer id,String name) {}
+public record Product(String name) {
+}
